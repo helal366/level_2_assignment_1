2f58535f9acd9ef02845c4e6dce99b329abf6b00
Helal366 <helal56876@gmail.com>
2025-09-23 09:54:15 +0600

    Problem five solved. The previous commit should be the solution of four.

---
+++ src/5_problemFive.ts
@@ -0,0 +1,13 @@
+{
+    // Solution of Problem five
+    const processValue=(value: string|number):number=>{
+        if(typeof value==='string'){
+            return value.length;
+        }else{
+            return value*2;
+        };
+    }
+    console.log(processValue('The shephard boy felt lonely and dull.'))
+    console.log(processValue(10))
+    // 
+}
