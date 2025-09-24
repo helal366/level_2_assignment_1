1ab4c47143b49fae2b71ab67675095c088c0f3ed
Helal366 <helal56876@gmail.com>
2025-09-24 09:45:12 +0600

    Revised.

--- src/1_problemOne.ts
+++ src/1_problemOne.ts
@@ -1,13 +1,10 @@
 {
     // 
     const formatString = (param1:string, param2?: boolean):string=>{
-        if(param2===true){
-            return param1.toUpperCase()
-        }else if(param2===false){
+        if(param2===false){
             return param1.toLowerCase()
-        }else{
-            return param1.toUpperCase()
         }
+        return param1.toUpperCase()
     }
     console.log(formatString('The hungry fox is wandering through the forest in search of food.')) 
     console.log(formatString('Suddenly he saw a vineyard with brunches full of bunches fresh, juicy and purple grapes.', true)) 
