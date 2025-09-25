1cc7257fc523991810980e2c9cf5a3061c0a7ffd
Helal366 <helal56876@gmail.com>
2025-09-25 09:41:44 +0600

    Problem one revised.

--- src/1_problemOne.ts
+++ src/1_problemOne.ts
@@ -1,13 +1,15 @@
 {
     // 
-    const formatString = (param1:string, param2?: boolean):string=>{
-        if(param2===false){
-            return param1.toLowerCase()
+    const formatString = (input:string, toUpper?: boolean):string=>{
+        if(toUpper===false){
+            return input.toLowerCase()
         }
-        return param1.toUpperCase()
+        return input.toUpperCase()
     }
-    console.log(formatString('The hungry fox is wandering through the forest in search of food.')) 
-    console.log(formatString('Suddenly he saw a vineyard with brunches full of bunches fresh, juicy and purple grapes.', true)) 
-    console.log(formatString('The fox went there and thought, "If i can eat the grapes, my hunger will go away."', false)) 
+    console.log(formatString('Hello')) 
+    console.log(formatString('Hello', true)) 
+    console.log(formatString('Hello', false)) 
+    
     // 
 }
+
